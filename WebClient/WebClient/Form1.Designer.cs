68ad8d4be49828b4db0064c8f491ecad5cbe89f6
DKnight-ICE <609697617@qq.com>
2016-02-23 10:22:53 +0800

    第一次提交

---
+++ WebClient/WebClient/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WebClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(232, 44);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(444, 33);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(56, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "网址:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(59, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "图片显示:";
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(232, 167);
+            this.picBox.Margin = new System.Windows.Forms.Padding(6);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(550, 366);
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(747, 41);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(97, 43);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "显示";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 624);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "下载并显示网络图片";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ WebClient/WebClient/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WebClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            Uri uri = new Uri(this.txtUrl.Text, UriKind.Absolute);
+            System.Net.WebClient webClient = new System.Net.WebClient();
+
+            //方法一
+            //webClient.OpenReadAsync(uri);
+            //webClient.OpenReadCompleted += new OpenReadCompletedEventHandler(client_OpenReadCompleted);
+
+            //方法二
+            var response = webClient.OpenRead(uri);
+            if (response != null) this.picBox.Image=new Bitmap(response);
+            
+            //其它方法自行测试...
+        }
+
+        void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            Stream stream = e.Result;
+            Image img = new Bitmap(stream);
+            this.picBox.Image = img;
+        }
+    }
+}
